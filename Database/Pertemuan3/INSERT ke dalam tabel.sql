9950a0ad54dcaec089c6e5ac44792ff2b06adf15
BangDIMMMMMMM <dimassuryaputraaa@gmail.com>
2024-09-24 10:45:17 +0800

    Add files via upload

---
+++ Database/Pertemuan3/INSERT ke dalam tabel.sql
@@ -0,0 +1,7 @@
+INSERT INTO produk (nama_produk, kategori, harga, stok) 
+VALUES 
+('PC', 'Elektronik', 7500000.00, 10),
+('Handphone', 'Elektronik', 3500000.00, 25),
+('Meja Kayu', 'Furniture', 500000.00, 15),
+('Sepatu Lari', 'Fashion', 600000.00, 30),
+('Kulkas', 'Elektronik', 3200000.00, 8);
